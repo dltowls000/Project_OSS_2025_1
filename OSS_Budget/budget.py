da7a9bbad51203f18d0016cd86ce841668574e51
ChristmasTree <dltowls000@gmail.com>
2025-06-06 06:29:47 +0900

    [기능추가] 1000원 단위 구분자 적용 + 카테고리별 총액 pie chart를 통한 시각화

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -1,14 +1,25 @@
 import datetime
 from expense import Expense
+import matplotlib.pyplot as plt
 
 class Budget:
     def __init__(self):
         self.expenses = []
+        self.cate = []
+        self.cate_total = []
 
     def add_expense(self, category, description, amount):
         today = datetime.date.today().isoformat()
         expense = Expense(today, category, description, amount)
         self.expenses.append(expense)
+
+        if category not in self.cate:
+            self.cate.append(category)
+            self.cate_total.append(0)
+        
+        idx = self.cate.index(category)
+        self.cate_total[idx] += amount
+
         print("지출이 추가되었습니다.\n")
 
     def list_expenses(self):
@@ -22,6 +33,19 @@ class Budget:
 
     def total_spent(self):
         total = sum(e.amount for e in self.expenses)
-        print(f"총 지출: {total}원\n")
+        print(f"총 지출: {total:,}원\n")  
 
+    def show_graph(self, path=None):
+        fig = plt.figure(num="간단 가계부")
+        wedgeprops={'width': 0.7, 'edgecolor': 'w', 'linewidth': 3}
+        legend_labels = [f"{total:,}원" for total in self.cate_total]
+        labels =[f"{cat}" for cat in self.cate]
+        today = datetime.datetime.now()
 
+        plt.rcParams['font.family'] ='Malgun Gothic'
+        plt.rcParams['axes.unicode_minus'] = False
+        plt.title(today.strftime("<%Y년 %m월 가계부 카테고리별 지출 총액>"))
+        plt.pie(self.cate_total, labels=labels, autopct='%.2f%%', wedgeprops=wedgeprops)
+        plt.legend(legend_labels, title='카테고리별 지출 총액', fontsize=8, bbox_to_anchor=(0.9, 1.0))
+
+        plt.show()
--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -1,6 +1,5 @@
 from budget import Budget
 
-
 def main():
     budget = Budget()
 
@@ -9,7 +8,8 @@ def main():
         print("1. 지출 추가")
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
-        print("4. 종료")
+        print("4. 카테고리별 지출 총액 보기")
+        print("5. 종료")
         choice = input("선택 > ")
 
         if choice == "1":
@@ -17,6 +17,9 @@ def main():
             description = input("설명: ")
             try:
                 amount = int(input("금액(원): "))
+                if (amount <= 0):
+                    print("음수 혹은 0원은 입력할 수 없습니다.\n")
+                    continue
             except ValueError:
                 print("잘못된 금액입니다.\n")
                 continue
@@ -29,12 +32,17 @@ def main():
             budget.total_spent()
 
         elif choice == "4":
+            if not budget.expenses:
+                print("가계부가 비어있어 차트를 생성할 수 없습니다.\n")
+                continue
+            budget.show_graph()
+
+        elif choice == "5":
             print("가계부를 종료합니다.")
             break
 
         else:
             print("잘못된 선택입니다.\n")
-
 
 if __name__ == "__main__":
     main()
